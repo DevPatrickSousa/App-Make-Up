89bf0e1df3e5adcf3bb74dcc29ee9273ceecfc67
DevPatrickSousa <patrickseven22@hotmail.com>
2022-11-10 23:30:14 -0300

    updating views

--- src/pages/Splash/index.jsx
+++ src/pages/Splash/index.jsx
@@ -1,19 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function Splash() {
+  const [loaded] = useFonts({
+    nunito: require('../../assets/fonts/Nunito-VariableFont_wght.ttf'),
+    montserrat: require('../../assets/fonts/Montserrat-VariableFont_wght.ttf')
+  });
+
+
   return (
+
+
+
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Text>MINHA</Text>
+      <Text style={styles.minha}>MINHA</Text>
       <hr />
-      <Text>Makeup</Text>
-      
+      <Text style={styles.makeUp}>Makeup</Text>
+
       <Image source={require('../../assets/testeIcon.png')}
-   style={{width: 400, height: 400}} />
-      
-      <Text>Santana de Parnaíba</Text>
-      <Text>São Paulo</Text>
+        style={{ width: 400, height: 400 }} />
+
+      <Text style={styles.santanaParnaiba}>Santana de Parnaíba</Text>
+      <Text style={styles.saoPaulo}>São Paulo</Text>
     </View>
   );
 }
@@ -22,7 +32,32 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#EA9AB2',
-    alignItems:'center',
+    alignItems: 'center',
     justifyContent: 'center'
   },
+  makeUp: {
+    fontFamily: 'nunito',
+    fontSize: 48,
+    color: '#E0678B',
+    textShadowColor: '#0000', textShadowOffset: { width: '0px', height: '4px', blur:'4px' }
+  },
+  santanaParnaiba: {
+    fontFamily: 'Nunito-ExtraBold',
+    fontSize: 20,
+    color: '#FFFF'
+  },
+  saoPaulo: {
+    fontFamily: 'Nunito-SemiBold',
+    fontSize: 20,
+    color: '#FFFF'
+  },
+  minha:{
+    fontFamily: 'Montserrat-Regular',
+    fontSize:48,
+    letterSpacing: '10px',
+    lineHeight: '30px',
+    color: '#EFCFE3',
+    textShadowColor: '#0000', textShadowOffset: { width: '0px', height: '4px', blur:'4px' },
+    opacity: 0.98
+  }
 });
