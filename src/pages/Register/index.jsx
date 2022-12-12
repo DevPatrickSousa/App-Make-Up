3ea632295d12be0de3e047339a7df30e4141fca0
DevPatrickSousa <patrickseven22@hotmail.com>
2022-12-12 00:08:52 -0300

    add token auth for user register

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,17 +1,98 @@
+//imports
+
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Avatar } from 'react-native-paper';
 import InputComponent from '../../components/InputComponent/index';
 import ButtonComponent from "../../components/ButtonComponent/index";
-
+import api from '../../Services/api'
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 export default function Register() {
 
   //declaring function to register people data.
+  const [userName, setName] = useState(' ');
+  const [age, setAge] = useState(' ');
+  const [email, setEmail] = useState(' ');
+  const [password, setPassword] = useState(' ');
+  const [number, setNumber] = useState(' ');
+  var tokenAuth = '';
+  
 
-  function Register() {
-    alert('teste')
+  function getToken() {
+
+    return new Promise((resolve, reject) => {
+
+        let optionToken = {
+            headers: { "authentication": "4dm1n" }
+        }
+
+        api.get('/getToken?t=4dm1n', optionToken
+        ).then(
+            response => {
+                tokenAuth = response.data.token
+                resolve(tokenAuth)
+            }
+        )
+    })
+}
+
+  const navigation = useNavigation();
+  function goToHomePage() {
+    navigation.navigate('Home');
   }
+
+  function cleanInputs() {
+    setName(' ');
+    setAge(' ');
+    setEmail(' ');
+    setPassword(' ');
+    setNumber(' ');
+  }
+
+  
+
+
+  const userRegister = async () => {
+    var tokenAuth = await  getToken();
+    console.log(tokenAuth + 'teste')
+    let option = { headers: { 'Content-Type': ['application/json'], 'authorization': 'Bearer ' + tokenAuth } }
+    await api.post('/registration?key=4dm1n',{
+    nome:userName,
+    idade:age,
+    email:email,
+    senha:password,
+    telefone:number,
+  }, option).then(function (response) {
+    console.log(response);
+    alert('USUÁRIO REGISTRADO!')
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  
+
+
+    
+    
+  }
+
+
+  /* const userRegister = () => {
+     
+ 
+       console.log(userName);
+       console.log(age);
+       console.log(email);
+       console.log(password);
+       console.log(number);
+       alert('ok')
+       
+   }
+ */
+
   return (
     <View style={styles.container}>
 
@@ -20,43 +101,54 @@ export default function Register() {
 
       <Avatar.Icon icon="account" color="black" style={styles.logo} />
 
-
-      <InputComponent
-        leftIcon={{ type: 'font-awesome', name: 'chevron-left' }}
-        label='Nome'
-        placeholder='Digite o seu nome'
-
-      />
-
-      <InputComponent
-        label='Idade'
-        placeholder='Digite a sua idade'
-      />
-
-      <InputComponent
-        label='Email'
-        placeholder='Digite o seu email'
-      />
+      <form className='formRegister'>
 
 
-      <InputComponent
-        
-        label='Senha'
-        placeholder='Digite a sua senha'
-        
-      />
 
-      <InputComponent
-        label='Tel.:'
-        placeholder='Digite o seu telefone para contato'
-      />
+        <InputComponent
+          leftIcon={{ type: 'font-awesome', name: 'chevron-left' }}
+          label='Nome'
+          placeholder='Digite o seu nome'
+          onChange={(e) => setName(e.target.value)}
+          secureTextEntry={false}
+        />
 
-      <ButtonComponent
-        title="Cadastrar Dados"
-        onPress={Register} // when the button is clicked, data is registered in the data base.
+        <InputComponent
+          label='Idade'
+          placeholder='Digite a sua idade'
+          onChange={(e) => setAge(e.target.value)}
+          secureTextEntry={false}
+        />
 
-      />
+        <InputComponent
+          label='Email'
+          placeholder='Digite o seu email'
+          onChange={(e) => setEmail(e.target.value)}
+          secureTextEntry={false}
+        />
 
+
+        <InputComponent
+          label='Senha'
+          placeholder='Digite a sua senha'
+          onChange={(e) => setPassword(e.target.value)}
+          secureTextEntry={true}
+        />
+
+        <InputComponent
+          label='Tel.:'
+          placeholder='Digite o seu telefone para contato'
+          onChange={(e) => setNumber(e.target.value)}
+          secureTextEntry={false}
+        />
+
+        <ButtonComponent
+          title="Cadastrar Dados"
+          onPress={()=>{userRegister();cleanInputs();}} // when the button is clicked, data is registered in the data base.
+          
+          
+        />
+      </form>
     </View>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ import Profile from './pages/Profile';
 import UsefulInformations from './pages/Useful_Information';
 import Laws from './pages/Laws';
 import EmergencyContacts from "./pages/emergency_Contacts";
+import Login from "./pages/Login";
 
 
 const Drawer = createDrawerNavigator();
@@ -19,12 +20,13 @@ function MyDrawer() {
   return (
     //content of menu.
     <Drawer.Navigator>
+      <Drawer.Screen name="Register" component={Register} />
       <Drawer.Screen name="Home" component={Home} />
       <Drawer.Screen name="Profile" component={Profile} />
       <Drawer.Screen name="Splash" component={Splash} />
-      <Drawer.Screen name="Register" component={Register} />
       <Drawer.Screen name="Usefulnformations" component={UsefulInformations} />
       <Drawer.Screen name="Laws" component={Laws} />
+      <Drawer.Screen name="Login" component={Login} />
       <Drawer.Screen name="EmergencyContacts" component={EmergencyContacts} options={{ drawerItemStyle: { display: "none" } }} />
     </Drawer.Navigator>
   );
